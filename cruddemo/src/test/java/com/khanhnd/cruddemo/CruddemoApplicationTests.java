407cd79ba06b6b6702c0c84b02cd9c169b9b24c1
Khanh Nguyen Duy <khanhnguyenduy@Khanhs-MacBook-Pro.local>
2025-06-27 14:44:27 +0700

    Add cruddemo folder

---
+++ cruddemo/src/test/java/com/khanhnd/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khanhnd.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
